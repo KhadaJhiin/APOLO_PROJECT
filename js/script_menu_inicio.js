2c863f94a304a5c06c9c0571b819b2b624924005
KhadaJhiin <josemiguelortegagaviria@gmail.com>
2023-11-22 17:37:16 -0500

    Se sube sidebar agregando menu desplegable

--- js/script_menu_inicio.js
+++ js/script_menu_inicio.js
@@ -1,3 +1,18 @@
+let listElements = document.querySelectorAll('.links__button--click');
+listElements.forEach(listElement => {
+    listElement.addEventListener('click' , ()=>{
+        listElement.classList.toggle('arrow');
+
+        let height =0;
+        let menu = listElement.nextElementSibling;
+        
+        if(menu.clientHeight =="0"){
+            height=menu.scrollHeight;
+        }
+        menu.style.height = `${height}px`
+    })
+});
+
 const body = document.querySelector('body'),
         sidebar=body.querySelector('nav'),
         toggle=body.querySelector(".toggle");
